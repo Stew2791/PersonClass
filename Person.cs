4acedd34447d89b6b29e612afcf848d27db0be13
Stewart W <st3w@protonmail.com>
2024-10-22 09:42:06 +0100

    Update Person.cs
    Minor tweaks.

--- Person.cs
+++ Person.cs
@@ -5,6 +5,7 @@
 
  Todo...
  - add some basic validation for names and addresses?, ie. minimum string lengths.
+ - add some basic validation for the e-mail address, ie. contains an '@' symbol and looks like a valid domain name.
 
 */
 
@@ -21,7 +22,7 @@ namespace MyClasses
         private DateTime _dateOfBirth;
         private string _phoneNumber;
 
-        // public 'automatic properties', the compiler will create hidden backing fields...
+        // public 'automatic properties', the compiler will create hidden backing fields for them...
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
@@ -51,22 +52,23 @@ namespace MyClasses
             set
             {
                 /*
-                 set the backing field to a default fail value that will be replaced after successful validation...
-                 ie. if the value supplied is "banana" then it will end up with the default value instead.
+                 set the backing field to a default value that will be replaced after successful validation...
+                 ie. if the value supplied is invalid then it will end up with the default value instead.
+                 nb. the value in this case is obviously a string.
                 */
                 _phoneNumber = defaultString;
 
                 if (value.Length > 8 && value.Length < 20)
                 {
-                    bool allValidChars = true;
+                    bool looksOk = true;
 
                     for (int i = 0; i < value.Length; i++)
                     {
                         if (!Char.IsDigit(value[i]) && !Char.IsWhiteSpace(value[i]))
-                            allValidChars = false;
+                            looksOk = false;
                     }
 
-                    if (allValidChars)
+                    if (looksOk)
                         _phoneNumber = value;
                 }
             }
@@ -113,17 +115,6 @@ namespace MyClasses
             Console.WriteLine("Date of Birth: {0}\\{1}\\{2} ({3} yrs)", DateOfBirth.Day, DateOfBirth.Month, DateOfBirth.Year, age);
         }
 
-        /*
-         returns the person's age (in days) calculated from their birthday and the current date.
-         not especially useful but interesting nonetheless.
-        */
-        public int GetAgeInDays()
-        {
-            int age = (DateTime.Now - DateOfBirth).Days;
-
-            return age;
-        }
-
         // returns the person's age (in years) calculated from their birthday and the current date.
         public int GetAgeInYears()
         {
@@ -138,6 +129,17 @@ namespace MyClasses
                 if (DateTime.Now.Day < DateOfBirth.Day)
                     age--;
             }
+
+            return age;
+        }
+
+        /*
+         returns the person's age (in days) calculated from their birthday and the current date.
+         not especially useful but interesting nonetheless.
+        */
+        public int GetAgeInDays()
+        {
+            int age = (DateTime.Now - DateOfBirth).Days;
 
             return age;
         }
